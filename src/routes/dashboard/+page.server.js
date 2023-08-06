d5cf09e8a5a0285a11b0a4fa94def4d634cd2b35
Guru18650 <guruwszystkiego@gmail.com>
2023-08-06 17:21:11 +0200

    download link fix

--- src/routes/dashboard/+page.server.js
+++ src/routes/dashboard/+page.server.js
@@ -1,5 +1,8 @@
 import { protectedPage } from "$lib/api";
+import { apiFetch } from "$lib/api";
 
 export async function load({cookies}){
-    await protectedPage(cookies);
+    let rows = await apiFetch('/misc/download',{});
+    let link = rows.data.data.adress;
+    return {link};
 }
--- src/routes/dashboard/+page.svelte
+++ src/routes/dashboard/+page.svelte
@@ -1,8 +1,11 @@
+<script>
+    export let data;
+</script>
 <div class="p-6 card !bg-transparent rounded-2xl shadow-4xl sm:max-w-xl m-auto md:mt-32 mt-16 text-center">
     <h1 class="text-xl leading-tight text-center tracking-tight md:text-3xl">
         Welcome to Unreal Kingdoms!
     </h1>
-    <a class="btn variant-filled-primary w-full mt-5" href="/">Download Unreal Kingdoms</a>
+    <a class="btn variant-filled-primary w-full mt-5" href={data.link}>Download Unreal Kingdoms</a>
     <a class="btn variant-filled-primary w-full mt-5" href="exchange">Exchange platform</a>
     <div class="btn bg-gray-400 w-full mt-5">Coming soon...</div>
 </div>
