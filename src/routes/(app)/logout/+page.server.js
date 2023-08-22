e7b8b5038aba266df141de63b1c9af8f9e1e57ea
Guru18650 <guruwszystkiego@gmail.com>
2023-08-22 03:58:25 +0200

    Update +page.server.js

--- src/routes/(app)/logout/+page.server.js
+++ src/routes/(app)/logout/+page.server.js
@@ -1,7 +1,7 @@
 import { redirect } from '@sveltejs/kit';
 
 export async function load({cookies}){
-    cookies.set("jwt","");
+    cookies.set("jwt","", {maxAge:-1});
     throw redirect(301,'/');
 
 }
